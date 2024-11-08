8245084f3b9970f4a133ccc8af03caa544026a2f
Italo Bruno <italo23042001@gmail.com>
2024-11-07 23:09:26 -0300

    Ciclo maior aqui foi

--- CicloMaior.c
+++ CicloMaior.c
@@ -5,33 +5,37 @@
 #define VISITING 1
 #define VISITED 2
 
-int dfs(int* edges, int* status, int node, int step) {
-    if (status[node] == step) {
-        return step - status[node];  
+int dfs(int* edges, int* status, int* visitOrder, int node, int step) {
+    if (status[node] == VISITING) {
+        return step - visitOrder[node];  
     }
-    if (status[node] > 0 || edges[node] == -1) {
+    if (status[node] == VISITED || edges[node] == -1) {
         return -1;  
     }
-    status[node] = step;  
-    int cycleLength = dfs(edges, status, edges[node], step + 1);  
-    status[node] = -1;  
+
+    status[node] = VISITING;          
+    visitOrder[node] = step;          
+    int cycleLength = dfs(edges, status, visitOrder, edges[node], step + 1);
+    status[node] = VISITED;           
 
     return cycleLength;
 }
 
 int longestCycle(int* edges, int edgesSize) {
-    int *status = (int*) calloc(edgesSize, sizeof(int));  
-    int longestCycleLength = -1;  
+    int *status = (int*) calloc(edgesSize, sizeof(int));       
+    int *visitOrder = (int*) calloc(edgesSize, sizeof(int));   
+    int longestCycleLength = -1;                             
 
     for (int i = 0; i < edgesSize; i++) {
         if (status[i] == UNVISITED) {  
-            int cycleLength = dfs(edges, status, i, 1);  
+            int cycleLength = dfs(edges, status, visitOrder, i, 1);  
             if (cycleLength > longestCycleLength) {
                 longestCycleLength = cycleLength;  
             }
         }
     }
 
-    free(status);  
+    free(status);
+    free(visitOrder); 
     return longestCycleLength;
 }
