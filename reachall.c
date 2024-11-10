b92b6c7c4f75c3ad7cf97f390fdd87fb5a27c2b8
Carlos Eduardo Mendes de Mesquita <58157127+CarlosEduardoMendesdeMesquita@users.noreply.github.com>
2024-11-10 02:02:14 -0300

    Tentando entender a questão
    Tentando entender as variáveis fornecias pela entrada da função pelo leetcode.

---
+++ reachall.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** n sao os vertices ou nos
+ * edges e o ponteiro da matriz das arestas 
+ * edgesSize acho que e a distancia entre arestas
+ * edgesColSize acho que e a contagem de ate qual aresta a outra atinge por ser um grafo direcionado
+ * returnSize acredito que seja o retorno do exercicio
+ * a funcao e a que o leetcode fornece em C, por isso escrevi esse destrinchamento, para tentar me orientar*/
+
+int* findSmallestSetOfVertices(int n, int** edges, int edgesSize, int* edgesColSize, int* returnSize) {
+    
+}
