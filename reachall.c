f3d050fb9d412f617daf1e77cdc3ed37137e2825
Carlos Eduardo Mendes de Mesquita <58157127+CarlosEduardoMendesdeMesquita@users.noreply.github.com>
2024-11-10 15:21:31 -0300

    Primeira tentativa em C
    Feita uma lógica e enviada no leetcode, deu erro na linha 20 no terminal do mesmo.

--- reachall.c
+++ reachall.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/** n sao os vertices ou nos
- * edges e o ponteiro da matriz das arestas 
+/** analise da funcao fornecida pelo leetcode:
+ * n sao os vertices ou nos
+ * edges e a matriz que indica a direcao de uma aresta
  * edgesSize acho que e a distancia entre arestas
- * edgesColSize acho que e a contagem de ate qual aresta a outra atinge por ser um grafo direcionado
+ * edgesColSize acho que e o vetor que armazena as direcoes
  * returnSize acredito que seja o retorno do exercicio
- * a funcao e a que o leetcode fornece em C, por isso escrevi esse destrinchamento, para tentar me orientar*/
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+
 
 int* findSmallestSetOfVertices(int n, int** edges, int edgesSize, int* edgesColSize, int* returnSize) {
+    int encaminha[edgesSize]; //salva se o vetor edges naquela posição mostra um caminho ou nao 
+    int resposta[n]; // salva os vertices sem caminhos que chegam nele
     
+    for(int i = 0; i <= n; i++){
+        edgesColSize[i] = encaminha[i]++;
+        }
+    for(int i = 0; i <= n; i++){
+        if(encaminha[i] == 0){
+            resposta[i] = (int) edges[i];
+        }
+        returnSize[i] = resposta[i];
+    }
+    
+    return returnSize;
 }
