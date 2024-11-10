b3d0d405d2aa2e39093f8b469d24be616faebe1f
Carlos Eduardo Mendes de Mesquita <58157127+CarlosEduardoMendesdeMesquita@users.noreply.github.com>
2024-11-10 19:20:49 -0300

    Segunda tentativa
    Tentei alterar a lógica utilizando um comando que achei na parte 5 da documentação(remove) e tentando usar set para o vetor, pois não me parece correto usar uma lista normal.

--- reachall.py
+++ reachall.py
@@ -2,41 +2,19 @@ from collections import deque # biblioteca para usar listas como filas
 from typing import List 
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        # tentativa que vejo como gambiarra: transformar a lista de booleanos em um dicionario, pelo que vi
-        # na documentacao, ele e usado quando se precisa de chaves, nao sei se vai bastar para corrigir a 
-        # falha no leetcode, mas vou tentar.
-        
-        caminho = {}
-        
-        for i in range(n):
-            
-            caminho[i] = False
-           
-            # o problema pede que seja entregue os nos que nao possuem caminhos direcionados a ele, como e um par ordenado
-            # decidi por usar um vetor de booleanos inicializado como falso, representando que nao existem caminhos
-            # de forma que a solucao devera trazer exatamente os que nao sao acessiveis como resposta, pois e o que
-            # ocorre nos exemplos entregues pelo exercicio
-        
-        # usando a documentacao do proprio python, ele me traz o for de C como range ao meu ver, e o for para mim e mais 
-        # para trabalhar em cima de uma variavel ja entregue, nesse caso a lista encadeada edges entregue pela funcao 
+       # segunda tentativa, nao mudei a logica, mas tentarei usar um tipo de estrutura
+       # que achei na documentacao chamado set, como se fosse um vetor, nao entendi direito a estrutura,
+       # porem nao acho que um vetor usual ja funcionaria aqui
+        grafo = set()
 
-        for aresta in edges :
-            # criando as variaveis from e to que aparecem no problema
-            
-            x,y = aresta
-            
-            # como x e o from, ele sempre tera uma direcao a partir dele, ou seja, todos os y, que sao to, podem ou nao atingir 
-            # todos os outros vertices, entao todos os vertices que aparecerem em y devem ser colocados como verdadeiro
-            # pois assim apenas os que nao possuem caminho ate si serao salvos, e eles sao a resposta
-
-            caminho[y] = True
-        
-        # agora criamos o retorno da funcao, onde todos que tiverem permanecido com o valor falso serao copiados para o mesmo,
-        # usando fila, por isso importei a biblioteca indicada na documentacao de Python deque
-
-        resposta = []
-
-        for vert in range(n) :
-            if not caminho[vert]:
-                resposta.append[vert]
-        return resposta
+        for i in range(n) :
+            grafo.add(i)
+        # nao consegui tentando fazer com booleano, vou tentar usando o comando remove que achei na documentacao do python
+        # em data structures
+        for edge in edges :
+            # edges e composto por 2 entradas, so estava citando uma na primeira tentativa, vou tentar colocar separado aqui
+            de = edge[0]
+            para = edge[1]
+            if para in grafo :
+                grafo.remove(para)
+        return grafo
